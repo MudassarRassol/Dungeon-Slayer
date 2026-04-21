b172c6a2b0ca18fc5809d43f7ce4dd28d7df7961
unity00007 <unity00007@S9YPDGI549I2N2C>
2026-04-21 13:59:01 +0500

    push code

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        float run = Input.GetKey(KeyCode.LeftShift) ? 2f : 1f;
+        float run = Input.GetKey(KeyCode.LeftShift) ? 10f : 5f;
 
         Vector3 move = new Vector3(moveX, 0, moveZ);
 
@@ -27,5 +27,41 @@ public class PlayerController : MonoBehaviour
 
         anim.SetFloat("MoveX", moveX * run, 0.15f, Time.deltaTime);
         anim.SetFloat("MoveY", moveZ * run, 0.15f, Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Attack");
+            anim.SetTrigger("SW1");
+        }
+
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            Debug.Log("Attack2");
+            anim.SetTrigger("SW2");
+        }
+
+            if(Input.GetKeyDown(KeyCode.P))
+            {
+                Debug.Log("Attack3");
+                anim.SetTrigger("SW3");
+            }
+
+            if(Input.GetKeyDown(KeyCode.K))
+            {
+                Debug.Log("Shield Attack");
+                anim.SetTrigger("SA1");
+            }
+
+            if(Input.GetKeyDown(KeyCode.L))
+            {
+                Debug.Log("Shield Attack2");
+                anim.SetTrigger("SA2");
+            }
+
+             if(Input.GetKeyDown(KeyCode.Semicolon))
+            {
+                Debug.Log("Shield Attack3");
+                anim.SetTrigger("SW3");
+            }
     }
 }
